1ae88dead6c0a30cc155990792ea0fad29a6a812
Vynicius15 <81839894+Vynicius15@users.noreply.github.com>
2021-04-05 15:46:36 -0300

    adicionando main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-Our main file. 
+#Our main file.
+
+isport speech_recognition as sr 
+
+# cria um reconhecedor
+r = sr.recognizer ()
+
+# Abrir o microfone para captura
+with sr. microfone() as sowrce:
+   audio - r.listen(sowrce) # Define microfone como fonte de áudio 
+
+with true: 
+print(r. recognize_google(audio))
